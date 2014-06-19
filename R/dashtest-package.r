0c7f11e054c5e99b894cd248e9a62bcbf078ada6
tdschenk <schenk.tyler@gmail.com>
2014-06-19 11:51:08 -0500

    undo change

--- R/dashtest-package.r
+++ R/dashtest-package.r
@@ -5,7 +5,7 @@
 NULL
 
 #' @export
-plottest <- function(data, params, ...) {
+mapplot <- function(data, params, ...) {
   data$lat <- data$lat - 70
   map <- get_googlemap(center = c(lon = 50, lat = 30), zoom = 6)
   ggmap(map) + geom_point(aes(x = lon, y = lat, color = pt), 
