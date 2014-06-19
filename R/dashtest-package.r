405b8a963ba6938f4262250dcd8d2abcb47aac04
tdschenk <schenk.tyler@gmail.com>
2014-06-19 11:38:38 -0500

    mapplot function

--- R/dashtest-package.r
+++ R/dashtest-package.r
@@ -5,6 +5,9 @@
 NULL
 
 #' @export
-plottest <- function(df, ...) {
-  plot(df$lat ~ df$lon)
+mapplot <- function(data, params, ...) {
+  data$lat <- data$lat - 70
+  map <- get_googlemap(center = c(lon = 50, lat = 30), zoom = 6)
+  ggmap(map) + geom_point(aes(x = lon, y = lat, color = pt), 
+                          data = data)
 }
