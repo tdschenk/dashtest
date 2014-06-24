26b7b945dc140f977288f96c87f35cf7f019aa4b
Erik Iverson <erik@sigmafield.org>
2014-06-24 15:06:43 -0500

    can now select participant

--- demo.js
+++ demo.js
@@ -1,34 +1,49 @@
-// ocpu.seturl("http://107.170.188.61/ocpu/github/erikriverson/pilrdash/R");
 
-// ocpu.rpc("pilr_dashboard_panel", 
-//          { "FUN" : "vistest",
-//            "package" : "dashtest",
-//            "package_location" : "github",
-//            "pilr_server" : "qa.pilrhealth.com", 
-//            "access_code" : "4efe6ea3-a2e5-41bc-964e-63ebf09ae851",
-//            "data" : {"data_set" : "pilrhealth:location_ref_app:location",
-//                      "schema" : "1"},
-//            "github_info" : {"username" : "tdschenk", 
-//                             "repository" : "dashtest", 
-//                             "branch" : "master"},
-//            "project" : "a-eiverson-project", 
-//            "return_type" : "vega"
-//          }, 
-//          function(output) {
-//              console.log(JSON.parse(output));
-//              ggvis.getPlot("ggvis_univariate").parseSpec(JSON.parse(output));
-//          });
+angular.module('pilrdemo', ['ui.bootstrap']);
+
+ocpu.seturl("http://107.170.188.61/ocpu/github/erikriverson/pilrdash/R");
+
+
+var update_dash = function(part) {
+    var data;
+    if(part !== 'All') {
+        data = {"participant" : part,
+                    "data_set" : "pilrhealth:location_ref_app:location",
+                    "schema" : "1"};
+    } else {
+        data = {"data_set" : "pilrhealth:location_ref_app:location",
+                "schema" : "1"};
+    }
+
+ocpu.rpc("pilr_dashboard_panel", 
+         { "FUN" : "vistest",
+           "package" : "dashtest",
+           "package_location" : "github",
+           "pilr_server" : "qa.pilrhealth.com", 
+           "access_code" : "4efe6ea3-a2e5-41bc-964e-63ebf09ae851",
+           "data"        : data, 
+           "github_info" : {"username" : "tdschenk", 
+                            "repository" : "dashtest", 
+                            "branch" : "master"},
+           "project" : "a-eiverson-project", 
+           "return_type" : "vega"
+         }, 
+         function(output) {
+             console.log(JSON.parse(output));
+             ggvis.getPlot("ggvis_univariate").parseSpec(JSON.parse(output));
+         });
+};
 
 
  var DropdownCtrl = function($scope) {
-  $scope.items = [
-    'The first choice!',
-    'And another choice for you.',
-    'but wait! A third!'
-  ];
+  $scope.selected = function(choice) {
+      update_dash(choice);
+  };
+
+  $scope.items = ['All', 't01', 't02'];
 
   $scope.status = {
-    isopen: true
+    isopen: false
   };
 
   $scope.toggled = function(open) {
