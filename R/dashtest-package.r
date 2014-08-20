fe99878dee1013426d079910cd9ec18a215593b1
tdschenk <schenk.tyler@gmail.com>
2014-08-20 09:37:23 -0500

    axis rotation syntax

--- R/dashtest-package.r
+++ R/dashtest-package.r
@@ -22,7 +22,8 @@ scattervis <- function(data, params, ...) {
           fill = ~pt
     ) %>%
     layer_points() %>%
-    add_axis("x", title = "Latitude", labels = list(angle = 45)) %>%
+    add_axis("x", title = "Latitude", 
+             properties = axis_props(labels = list(angle = 45))) %>%
     add_axis("y", title = "Longitude")
 }
 
@@ -41,7 +42,8 @@ records.per.day <- function(data, params, ...) {
   a %>%
     ggvis(x = ~Group.1, y = ~count, fill := "tan") %>%
     layer_bars() %>%
-    add_axis("x", title = "Day", labels = list(angle = 45)) %>%
+    add_axis("x", title = "Day",
+             properties = axis_props(labels = list(angle = 45))) %>%
     add_axis("y", title = "Number of Records")
 }
 
@@ -53,7 +55,8 @@ records.per.hour <- function(data, params, ...) {
   dt %>%
     ggvis(x = ~Var1, y = ~Freq, fill := "tan") %>%
     layer_bars() %>%
-    add_axis("x", title = "Hour", labels = list(angle = 45)) %>%
+    add_axis("x", title = "Hour",
+             properties = axis_props(labels = list(angle = 45))) %>%
     add_axis("y", title = "Number of Records")
 }
 
@@ -64,7 +67,8 @@ steps.per.day <- function(data, params, ...) {
   data %>%
     ggvis(x = ~activities.steps.dateTime, y = ~activities.steps.value) %>%
     layer_lines() %>%
-    add_axis("x", title = "Day", labels = list(angle = 45)) %>%
+    add_axis("x", title = "Day", 
+             properties = axis_props(labels = list(angle = 45))) %>%
     add_axis("y", title = "Steps")
 }
 
